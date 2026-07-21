d3dfa05917d9c1f4ac7c6e12dd5b148e26ca6d1d
Ameer <myuguda175@gmail.com>
2026-07-21 22:48:10 +0400

    fixed radio selector id

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 5,
+  },
+  {
+    productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
+    quantity: 2,
+  },
+];
 
 export function addToCart(productId){
  let matchingItem;
